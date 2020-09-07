d268fd0a2816910c1c3e25c3bbb99abcee67c980
shubwub <shubwub@gmail.com>
2020-09-07 09:49:16 +0100

    adds loading spinner when fetching topics

--- src/components/molecules/TopicList.tsx
+++ src/components/molecules/TopicList.tsx
@@ -1,27 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
 	StyleSheet,
 	View,
 	FlatList,
 	RefreshControl,
-	Alert,
-} from 'react-native';
-import { mainDark, mainColour, red } from '../../../style_variables';
-import { getTopics } from '../../services/api';
-import { TopicInterface } from '../../interfaces';
-import { TopicCard } from '../atoms';
-import NewThread from '../atoms/NewThread';
+	Alert
+} from "react-native";
+import { mainDark, mainColour, red } from "../../../style_variables";
+import { getTopics } from "../../services/api";
+import { TopicInterface } from "../../interfaces";
+import { TopicCard } from "../atoms";
+import NewThread from "../atoms/NewThread";
+import Loading from "../atoms/Loading";
 
 export default function TopicList({
-	distance = 'furthest',
-	navigation,
+	distance = "furthest",
+	navigation
 }: {
 	distance: string;
 	navigation: any;
 }) {
 	const [topics, setTopics] = useState<TopicInterface[]>([]);
-  const [refreshing, setRefreshing] = useState<boolean>(false);
-  const [page, setPage] = useState<number>(1);
+	const [refreshing, setRefreshing] = useState<boolean>(false);
+	const [page, setPage] = useState<number>(1);
+	const [loading, setLoading] = useState<boolean>(true);
 
 	useEffect(() => {
 		const fetchTopics = async () => {
@@ -30,31 +32,32 @@ export default function TopicList({
 					const data: TopicInterface[] = await getTopics(
 						distance,
 						-106.018,
-            34.542,
-            page
-          );          
+						34.542,
+						page
+					);
 					setTopics(data);
+					setLoading(false);
 				},
-				(error) => Alert.alert(error.message),
+				error => Alert.alert(error.message),
 				{ enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 }
 			);
 		};
 		fetchTopics();
-  }, []);
-  
-  useEffect(() => {
+	}, []);
+
+	useEffect(() => {
 		const fetchTopics = async () => {
 			navigator.geolocation.getCurrentPosition(
 				async ({ coords: { latitude, longitude } }) => {
 					const data: TopicInterface[] = await getTopics(
 						distance,
 						-106.018,
-            34.542,
-            page
+						34.542,
+						page
 					);
 					setTopics([...topics, ...data]);
 				},
-				(error) => Alert.alert(error.message),
+				error => Alert.alert(error.message),
 				{ enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 }
 			);
 		};
@@ -66,37 +69,39 @@ export default function TopicList({
 	);
 
 	const onRefresh = async () => {
-    setRefreshing(true);
-    setPage(1)
+		setRefreshing(true);
+		setPage(1);
 		navigator.geolocation.getCurrentPosition(
 			async ({ coords: { latitude, longitude } }) => {
 				const data: TopicInterface[] = await getTopics(
 					distance,
 					-106.018,
-          34.542,
-          page
+					34.542,
+					page
 				);
 				setTopics(data);
 			},
-			(error) => Alert.alert(error.message),
+			error => Alert.alert(error.message),
 			{ enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 }
 		);
 		setRefreshing(false);
 	};
 
-	return (
+	return loading ? (
+		<Loading items="topics" />
+	) : (
 		<>
 			<View style={styles.listContainer}>
 				<FlatList
 					data={topics}
 					renderItem={renderItem}
-					keyExtractor={(item) => item.id.toString()}
+					keyExtractor={item => item.id.toString()}
 					style={styles.list}
 					refreshControl={
 						<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-          }
-          onEndReached={(() => setPage(page + 1))}
-          onEndReachedThreshold={0.3}
+					}
+					onEndReached={() => setPage(page + 1)}
+					onEndReachedThreshold={0.3}
 				/>
 			</View>
 			<NewThread navigation={navigation} />
@@ -107,7 +112,7 @@ export default function TopicList({
 const styles = StyleSheet.create({
 	listContainer: {
 		flex: 1,
-		backgroundColor: mainDark,
+		backgroundColor: mainDark
 	},
-	list: {},
+	list: {}
 });
