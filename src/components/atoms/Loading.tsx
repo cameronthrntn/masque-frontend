28e61df4a9800a33544a33ae22613f7a86b728e5
shubwub <shubwub@gmail.com>
2020-09-08 19:50:35 +0100

    extracts loading icon to be more reusable

--- src/components/atoms/Loading.tsx
+++ src/components/atoms/Loading.tsx
@@ -1,9 +1,15 @@
 import React, { useRef, useEffect } from "react";
 import Logo from "../../Images/masque_logo.svg";
-import { mainColour, mainDark } from "../../../style_variables";
-import { View, Text, StyleSheet, Animated } from "react-native";
+import { mainColour } from "../../../style_variables";
+import { StyleSheet, Animated } from "react-native";
 
-export default function Loading({ items }: { items: string }) {
+export default function Loading({
+	height,
+	width
+}: {
+	height: number;
+	width: number;
+}) {
 	const spinFlip = useRef(new Animated.Value(1)).current;
 
 	const duration = 200;
@@ -30,34 +36,10 @@ export default function Loading({ items }: { items: string }) {
 	}, []);
 
 	return (
-		<View style={styles.loadingContainer}>
-			<Animated.View
-				style={[
-					styles.logoContainer,
-					{ width: 75, transform: [{ scaleX: spinFlip }] }
-				]}
-			>
-				<Logo
-					style={styles.logo}
-					color={mainColour}
-					preserveAspectRatio="none"
-				/>
-			</Animated.View>
-		</View>
+		<Animated.View
+			style={[{ width, height, transform: [{ scaleX: spinFlip }] }]}
+		>
+			<Logo color={mainColour} preserveAspectRatio="none" />
+		</Animated.View>
 	);
 }
-
-const styles = StyleSheet.create({
-	loadingContainer: {
-		flex: 1,
-		flexDirection: "column",
-		justifyContent: "flex-start",
-		alignItems: "center"
-	},
-	logoContainer: {
-		height: 75,
-		marginBottom: 25,
-		marginTop: 50
-	},
-	logo: { width: "80%", height: 75 }
-});
