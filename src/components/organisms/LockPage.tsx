d729f93775457a7246e4f1a3614df6c5fc223b33
shubwub <shubwub@gmail.com>
2020-09-08 19:53:24 +0100

    changes lockpage to differ - comments and topics

--- src/components/organisms/LockPage.tsx
+++ src/components/organisms/LockPage.tsx
@@ -1,39 +1,57 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import { Lock } from '../../Images/icons';
-import { mainLight } from '../../../style_variables';
+import React from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { Lock } from "../../Images/icons";
+import { mainLight } from "../../../style_variables";
 
-export default function LockPage() {
+export default function LockPage({ internal = false }: { internal?: boolean }) {
 	return (
-		<View style={styles.wrapper}>
+		<View
+			style={[
+				styles.wrapper,
+				internal
+					? {
+							width: Dimensions.get("window").width,
+							height: Dimensions.get("window").height,
+							flexDirection: "column"
+					  }
+						: {
+							width: Dimensions.get("window").width,
+							flexDirection: "row",
+							opacity: 0.4,
+							bottom: 0,
+							left: 0,
+							top: 0,
+							right: 0
+					  }
+			]}
+		>
 			<Lock style={styles.lockIcon} />
-			<Text style={styles.lockText}>
-				This thread no longer exists. It can not be interacted with.
-			</Text>
+			{internal && (
+				<Text style={styles.lockText}>
+					This thread no longer exists. It can not be interacted with.
+				</Text>
+			)}
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	wrapper: {
-		position: 'absolute',
-		backgroundColor: 'black',
-		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height,
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center',
-		opacity: 0.6,
+		position: "absolute",
+		backgroundColor: "black",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		opacity: 0.6
 	},
 	lockIcon: {
-		height: 200,
-		width: 200,
+		height: 100,
+		width: 100
 	},
 	lockText: {
 		color: mainLight,
 		width: 200,
-		textAlign: 'center',
-		fontSize: 20,
-	},
+		textAlign: "center",
+		fontSize: 20
+	}
 });
