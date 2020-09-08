b8b2cd0be55f238d3c8870d29f73bae30aa0c7f3
shubwub <shubwub@gmail.com>
2020-09-08 19:51:35 +0100

    adds LockPage to organism exports

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,3 +1,4 @@
 export {default as TopicPage} from './TopicPage';
 export {default as CommentPage} from './CommentPage';
 export {default as TopicPost} from './TopicPost';
+export {default as LockPage} from './LockPage';
