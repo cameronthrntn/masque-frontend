81ed281306cb06ac67ea39c6915951a165b66afd
shubwub <shubwub@gmail.com>
2020-09-19 00:44:54 +0100

    adds mask interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -16,11 +16,17 @@ export interface CommentInterface {
   created_at: string;
   updated_at: string;
   mask_id: number;
-  topic_id: number;
   reply_id: number;
 }
 
 export interface CoordinatesInterface {
   latitude: number;
   longitude: number
+}
+
+export interface MaskInterface {
+  design: string;
+  colour: string;
+  user_id: number;
+  topic_id: number;
 }
