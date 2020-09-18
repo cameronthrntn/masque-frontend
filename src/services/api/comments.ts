aabce2162679ea4eb665a5ee52da31361309ac84
shubwub <shubwub@gmail.com>
2020-09-19 00:45:52 +0100

    adds postComment function

--- src/services/api/comments.ts
+++ src/services/api/comments.ts
@@ -11,3 +11,14 @@ export async function getComments(topic_id: number) {
     throw e;
   }
 }
+
+export async function postComment(comment: string, user_id: number, topic_id: number, reply_id: number) {
+  try {
+    const { data }: { data: CommentInterface } = await instance.post(
+      `/topics/${topic_id}/comments`, { comment, user_id, topic_id, reply_id }
+    );
+    return data;
+  } catch (e) {
+    throw e;
+  }
+}
